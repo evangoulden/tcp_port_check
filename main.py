0399ecc501b4ff5db04a7fb5d0f589ffd2fba3af
evan.goulden <evan.j.goulden@gmail.com>
2022-12-21 12:55:11 +0000

    Simple update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('127.0.0.1',80))
+result = sock.connect_ex(('192.168.255.70',23))
 if result == 0:
    print("Port is open")
 else:
