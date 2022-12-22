204b883601bc4aac3067e75bb05183b6f3ef3d4f
Evan Goulden <evan.goulden@mft.nhs.uk>
2022-12-22 15:45:18 +0000

    Added vEnv

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import socket
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('192.168.255.70',23))
+result = sock.connect_ex(('192.168.255.70', 23))
 if result == 0:
-   print("Port is open")
+    print("Port is open")
 else:
-   print("Port is not open")
+    print("Port is not open")
 sock.close()
